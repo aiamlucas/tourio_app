e2ee5111f8170870a5336c296b30ee000be50c98
Lucas Maia <lucasm@posteo.de>
2023-06-13 10:03:45 +0200

    picture display added

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,4 +1,3 @@
-//import { places } from '../../../../lib/db.js';
 import dbConnect from "../../../../db/connect.js";
 import Place from "../../../../db/models/Place.js";
 import useSWRMutation from "swr/mutation";
@@ -16,9 +15,7 @@ export default async function handler(request, response) {
     const placeToUpdate = await Place.findByIdAndUpdate(id, {
       $set: request.body,
     });
-    // Find the joke by its ID and update the content that is part of the request body!
     response.status(200).json(placeToUpdate);
-    // If successful, you'll receive an OK status code.
   }
 
   if (request.method === "GET") {
@@ -30,37 +27,8 @@ export default async function handler(request, response) {
 
     response.status(200).json(places);
 
-    // async function sendRequest(url, { arg }) {
-    //   const response = await fetch(url, {
-    //     methot: "PATCH",
-    //     body: JSON.stringify(arg),
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   });
-    //   if (response.ok) {
-    //     await response.json();
-    //   } else {
-    //     console.error(`Error: ${response.status}`);
-    //   }
-    // }
-
-    // const { trigger, isMutating } = useSWRMutation(
-    //   `/api/places/${id}`,
-    //   sendRequest
-    // );
-
     if (!id) {
       return;
     }
   }
 }
-
-//   const place = places.find((place) => place.id === id);
-
-//   if (!place) {
-//     return response.status(404).json({ status: 'Not found' });
-//   }
-
-//   response.status(200).json(place);
-// }
--- pages/places/[id]/edit.js
+++ pages/places/[id]/edit.js
@@ -20,49 +20,12 @@ export default function EditPage() {
         "Content-Type": "application/json",
       },
     });
-    // This syntax follows that of any regular HTTP response.
-    // Note the arg object that is passed as part of the response body.
+
     if (response.ok) {
       await response.json();
     } else {
       console.error(`Error: ${response.status}`);
     }
-    // const { trigger, isMutating } = useSWRMutation(
-    //   `/api/places/${id}`,
-    //   sendRequest
-    // );
-
-    // await trigger(placeData);
-
-    // async function sendRequest(url, { arg }) {
-    //   const response = await fetch(url, {
-    //     method: "PATCH",
-    //     body: JSON.stringify(arg),
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   });
-    //   if (response.ok) {
-    //     await response.json();
-    //   } else {
-    //     console.error(`Error: ${response.status}`);
-    //   }
-    // }
-
-    // const response = await fetch("/api/places", {
-    //   method: "PATCH",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(place),
-    // });
-
-    // if (response.ok) {
-    //   await response.json();
-    //   places.mutate();
-    // } else {
-    //   console.error(`Error: ${response.status}`);
-    // }
   }
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
